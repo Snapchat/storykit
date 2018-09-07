1ffc0e0268aaaee4009d90b82a9376e764158103
dliusc <dliu@snapchat.com>
2018-09-06 17:11:53 -0700

    Update config.js

--- client_example/javascript/config.js
+++ client_example/javascript/config.js
@@ -1,5 +1,5 @@
 export default {
-  Endpoint: 'https://kit.snap-dev.net/v1/stories/public',
+  Endpoint: 'https://api.snapkit.com/v1/stories/public',
   PrivateKeyFile: 'private_key.pem', // the path to your private key file
   CryptoAlgorithm: 'ES256',
   Issuer: 'dcb57664-94ba-469e-ab4c-e2468ad218b9', // <string: 3PA organization ID>
